ce03bf3122a2e1ae1d23af9d52514ea892607b94
Severo-creator <81829584+Severo-creator@users.noreply.github.com>
2024-11-10 14:29:59 -0300

    add

---
+++ Assets/GeradorTerreno.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerreinGenerator : MonoBehaviour
+{
+
+    public int depth = 20;
+
+    public int width = 256;
+    public int height = 256;
+
+    public float scale = 20;
+
+
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+
+    TerrainData GenerateTerrain(TerrainData terrainData){
+        terrainData.heightmapResolution = width + 1;
+
+        terrainData.size = new Vector3(width, depth, height);
+
+        terrainData.SetHeights(0, 0, GeneratHeights());
+        return terrainData;
+    }
+
+    float[,] GeneratHeights(){
+        float[,] heights = new float[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x,y] = CalculateHeight(x,y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x, int y){
+
+        float Xcoord = (float)x / width * scale;
+        float Ycoord = (float)y / height * scale;
+
+        return Mathf.PerlinNoise(Xcoord, Ycoord);
+    }
+}
